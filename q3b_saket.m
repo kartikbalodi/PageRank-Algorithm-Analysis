4520ae7a2abde642438d38fcfed7deedeb970d8e
Saket Agashe <32679555+saketagasheusc@users.noreply.github.com>
2020-11-08 13:15:28 -0800

    Update q3b_saket.m

--- q3b_saket.m
+++ q3b_saket.m
@@ -3,36 +3,44 @@ load('incidencematrix.mat');
 M = addPage(M);
 rnkDes = getCMatrix(M);
 
+% Initializing visited vector, keeps track of visited pages
+% Prevents adding same page twice
 visited = zeros(1001, 1)';
 visited(1001) = 1;
 curr_pos = 1001;
 
-%This method resolves in $987,849
-% cost = 0;
+% This method resolves in $1,972,504
+% Adds least costly pages first (Pages with cost to add of < $1000)
 % while curr_pos > 967
-%     M = addConnection(1001, rnkDes(curr_pos), M);
+%   M = addConnection(1001, rnkDes(curr_pos), M);
 % 	rnkDes = getCMatrix(M);
 % 	cost = cost + (1000 - rnkDes(curr_pos) + 1)^2;
-% 	if getRank(1001, rnkDes) > 0.1*length(M)
+%     curr_pos = curr_pos - 1;
+% 	if getRank(1001, rnkDes) < 0.1*length(M)
 %         break
 % 	end
 % end  
+% getRank(1001, rnkDes)
+% Adds new pages until the goal position is reached
 % while getRank(1001, rnkDes) > 0.1*length(M)
 %     M = addPage(M);
 %     M = addConnection(1001, length(M), M);
 %     rnkDes = getCMatrix(M);
 %     cost = cost + 1000;
 % end
+% getRank(1001, rnkDes)
 % cost
 
 
-%Greedy Algorithm - runs in $4983
+% Greedy Algorithm - runs in $68,806
+% Ranks each possible move by rank gained over cost
+% Chooses most efficient move based on highest rank gained over cost
 cost = 0;
-while getRank(1001, rnkDes) > 0.1*length(M)
-    greatest_step = -1;
-    greatest_step_index = -1;
-    currRank = getRank(1001, rnkDes);
-    for i = 1:length(visited)
+while getRank(1001, rnkDes) > 0.1*length(M) %Stopping condition - rank has been met
+    greatest_step = -1; %Initializing best rank-to-cost ratio to -1
+    greatest_step_index = -1; %Initializing index of ebst cost ratio to -1
+    currRank = getRank(1001, rnkDes); %Current rank of 1001
+    for i = 1:length(visited) % Calculates the best possible unvisited webpage to add a connectoin to
         currRank = getRank(1001, rnkDes);
         if visited(i) == 0
             A = M;
@@ -47,35 +55,34 @@ while getRank(1001, rnkDes) > 0.1*length(M)
         end
     end
     
+    % Calculates rank-to-cost ratio of adding a new webpage
     A = M;
     A = addPage(A);
-    A = addConnection(1001, length(M), M);
+    A = addConnection(1001, length(A), A);
     rnkDes = getCMatrix(A);
     theoretical_cost = 1000;
     rank_cost_ratio = (getRank(1001, rnkDes) - currRank)/theoretical_cost;
     
+    % Chooses the best rank-to-cost ratio step
     if rank_cost_ratio > greatest_step
-        a = "adding an edge"
         M = addPage(M);
         M = addConnection(1001, length(M), M);
         visited(length(M)) = 1;
         cost = cost + 1000;
         rnkDes = getCMatrix(M);
     else
-        b = greatest_step_index
         visited(greatest_step_index) = 1;
         M = addConnection(1001, greatest_step_index, M);
         rnkDes = getCMatrix(M);
         cost = cost + (1000 - greatest_step_index + 1)^2;
     end
-    
-    the_rank = getRank(1001, rnkDes)
-    cost
 end
+print(cost)
 
 
 
 % This method operates with $72,000
+% Only adding new webpages
 % cost = 0
 % while getRank(1001, rnkDes) > 0.1*length(M)
 %     M = addPage(M);
@@ -87,6 +94,8 @@ end
 
 
 % This method operates with $80,416
+% This methood adds the least costly method at each position (pure cost, no
+% consideration for rrank)
 % cost = 0
 % while getRank(1001, rnkDes) > 0.1*length(M)
 %     curr_cost = (1000 - curr_pos + 1)^2;
@@ -111,6 +120,7 @@ end
 % getRank(1001, rnkDes)
 % cost
 
+%Helper function made by Kartik to get the rnkDes vector
 function rnkDes = getCMatrix(M)
     A = M;
     A = normalizeCols(A);
@@ -123,18 +133,20 @@ function rnkDes = getCMatrix(M)
     rnkDes = flip(rnkAsc, 2);
 end
 
+%Helper function made to add connection from page j to page i in matrix A
 function A = addConnection(i, j, A)
     if A(i, j) == 0
         A(i, j) = 1;
     end
 end
 
+%Helper function to add a page to matrix A
 function B = addPage(A)
     B = A;
     B(length(A) + 1, length(A) + 1) = 0;
 end
 
-
+%Helper function made by Kartik to normalize columns of matrix A
 function A = normalizeCols(A)
     colSum = sum(A); % sum of elements in every column
     % normalize column vector elements so their sum-by-column adds up to 1
@@ -145,6 +157,7 @@ function A = normalizeCols(A)
     end
 end
 
+%Helper function made to get the current rank of webpage n
 function x = getRank(n, A)
     for i = 1:length(A)
         if A(i) == n
